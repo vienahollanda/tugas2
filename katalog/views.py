6584761dcb82c74a6439b67a2b0ce8b0157ad217
Viena Hollanda <viena.hollanda@ui.ac.id>
2022-09-14 20:15:48 +0700

    perubahan baru

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from katalog.models import CatalogItem
 # TODO: Create your views here.
+
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Viena Hollanda Koswara',
+        'NPM': '2106751253',
+    }
+    return render(request, "katalog.html", context)
